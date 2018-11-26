aaf65447504ce6dc6593d641c1ea0308a7debfa6
Hugh's Macbook <hughiscool@HughsdeMacBook-Pro.local>
2018-11-25 22:10:01 -0500

    added html

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def login(request):
+    return render(request, 'personal/basic.html', {'content':['Login Right Now!']})
+
+def register(request):
+    return render(request, 'personal/basic2.html', {'content': ['Register Right Now!']})
+
+def cancelbtn(request):
+    return render(request, 'personal/home.html')
